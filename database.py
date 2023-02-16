f39d72e08cb8b5a695a731466851401c78d09247
Shashwat Patel <113454648+ShashwatPatel107@users.noreply.github.com>
2023-02-16 01:13:50 +0000

    connect to the new MySQL database

--- database.py
+++ database.py
@@ -1,23 +1,26 @@
 import os
 import sqlalchemy
-from sqlalchemy import create_engine,engine , text
+from sqlalchemy import create_engine, engine, text
 # connect to my sql with sqlalchemy
-connection_info = os.environ['CONNECTION_INFO']
 
 
-# args for secure connection
-engine = create_engine(connection_info, connect_args={"ssl": {
-            "ssl_ca": "/etc/ssl/cert.pem"
-        }
-    })
+# database information goes here
+# HOST, USER, Pass
+connection_string = "mysql+pymysql://50tw8c2rvl6qzgut2xrw:pscale_pw_lFhP4LmriCPcBFAevspt9B6d2zaTBay9tlVVN40gFZ0@us-east.connect.psdb.cloud/shashwat_projects?charset=utf8mb4"
 
-def db_project(): 
-# Starting new connection 
+engine = create_engine(connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+
+def db_project():
+  # Starting new connection
   with engine.connect() as connection:
-    result = connection.execute(text("SELECT * FROM `projects`"))
-  
+    result = connection.execute(text("SELECT * FROM `shashwat_project`"))
+
   projects = []
   for row in result.all():
     projects.append(dict(row))
-
+    # Projects is rows of database table
   return projects
